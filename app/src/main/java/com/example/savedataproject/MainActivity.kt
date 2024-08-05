8c15955bf3802491afe90f353b5b97183175c61a
yavuzmutlu <yavuzmutlu41@gmail.com>
2024-08-05 20:00:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/savedataproject/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.savedataproject
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.savedataproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    //SharedPreferences
+    lateinit var sharedPreferences: SharedPreferences
+    var alinanKullaniciAdi: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+         sharedPreferences = this.getSharedPreferences("com.example.savedataproject",
+            Context.MODE_PRIVATE)
+
+         alinanKullaniciAdi = sharedPreferences.getString("isim", "")
+        if(alinanKullaniciAdi == "") {
+            binding.textView.text = "Kaydedilen isim: "
+        } else {
+            binding.textView.text = "Kaydedilen isim: ${alinanKullaniciAdi}"
+        }
+
+    }
+
+    fun kaydet(view: View) {
+        val kullaniciIsmi = binding.editText.text.toString()
+        if (kullaniciIsmi == "") {
+            Toast.makeText(this@MainActivity, "İsim kısmını boş bırakmayınız.", Toast.LENGTH_LONG)
+                .show()
+        } else {
+            sharedPreferences.edit().putString("isim", kullaniciIsmi).apply()
+            binding.textView.text = "Kaydedilen İsim: ${kullaniciIsmi}"
+        }
+    }
+
+    fun sil(view: View) {
+
+        alinanKullaniciAdi = sharedPreferences.getString("isim", "")
+        if (alinanKullaniciAdi != "") {
+            sharedPreferences.edit().remove("isim").apply()
+        }
+        binding.textView.text = "Kaydedilen isim: "
+    }
+}
